c22417fa22504e2509f1fb4a41f567ba7fb1e8bb
Jaimeson Waugh <jaimeson@4cesi.com>
2018-11-08 16:11:22 -0800

    Create main code

--- src/HTMLMailLink.php
+++ src/HTMLMailLink.php
@@ -3,10 +3,16 @@
 declare( strict_types = 1 );
 namespace WaughJ\HTMLMailLink
 {
-	class HTMLMailLink
+	use WaughJ\HTMLLink\HTMLLink;
+	use function WaughJ\TestHashItem\TestHashItemExists;
+
+	class HTMLMailLink extends HTMLLink
 	{
-		public function __construct()
+		public function __construct( string $email, array $other_attributes = [] )
 		{
+			$href = "mailto:$email";
+			$value = TestHashItemExists( $other_attributes, 'value', $email );
+			parent::__construct( $href, $value, $other_attributes );
 		}
 	}
 }
--- tests/HTMLMailLinkTest.php
+++ tests/HTMLMailLinkTest.php
@@ -5,9 +5,23 @@ use WaughJ\HTMLMailLink\HTMLMailLink;
 
 class HTMLMailLinkTest extends TestCase
 {
-	public function testObjectWorks()
+	public function testBasicMailLink()
 	{
-		$object = new HTMLMailLink();
-		$this->assertTrue( is_object( $object ) );
+		$link = new HTMLMailLink( 'waughjai@gmail.com' );
+		$this->assertEquals( $link->getHTML(), '<a href="mailto:waughjai@gmail.com">waughjai@gmail.com</a>');
+	}
+
+	public function testMailLinkWithCustomValue()
+	{
+		$link = new HTMLMailLink( 'waughjai@gmail.com', [ "value" => "Send me an email." ] );
+		$this->assertEquals( $link->getHTML(), '<a href="mailto:waughjai@gmail.com">Send me an email.</a>');
+	}
+
+	public function testMailLinkWithCustomValueAndOtherLinkAttributes()
+	{
+		$link = new HTMLMailLink( 'waughjai@gmail.com', [ "value" => "Send me an email.", "class" => "email-link" ] );
+		$this->assertContains( ' href="mailto:waughjai@gmail.com', $link->getHTML() );
+		$this->assertContains( ' class="email-link', $link->getHTML() );
+		$this->assertThat( $link->getHTML(), $this->logicalNot( $this->stringContains( ' value="Send me an email."' ) ) );
 	}
 }
